21697d1e367d103fd55896406aea342faaa001ba
Mehul <bechra.mehul@gmail.com>
2018-11-28 14:04:21 -0800

    Added front end - Main page and Create Account

--- index.js
+++ index.js
@@ -4,16 +4,15 @@
 
 // Dependencies
 const server = require('./lib/server');
-//const workers = require('./lib/workers');
 
 // Container for the app
 const app = {};
 
 // Init function
 app.init = () => {
-    // Start server and background workers
-    server.init();
-    //workers.init();
+  // Start server and background workers
+  server.init();
+  // workers.init();
 };
 
 // Execute init function
